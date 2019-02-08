28af5ca096cee8db7f9d35f1e948551faf4ca3d4
descomkraneoz <36744638+descomkraneoz@users.noreply.github.com>
2019-02-08 17:36:29 +0100

    Add files via upload

--- POO/Herencia/src/Disco.java
+++ POO/Herencia/src/Disco.java
@@ -2,14 +2,23 @@ public class Disco extends Multimedia {
 
     private final String compania;
 
+    enum Genero {
+        Blues, Corrido, Country, Cumbia, Disco, Electrónica, Flamenco, Folk, Funk, Gospel, Heavy_Metal, Hip_Hop,
+        Indie​, Jazz​, Merengue, Pop, Punk, Ranchera, Rap, Reggae, Reggaeton, Rumba, Rhythm_and_Blues, Rock,
+        Rock_and_Roll, Salsa, Son, Soul, Tango, Vallenato;
+    }
 
-    public Disco(String titulo, String autor, String compania, int duracion, Formato formato) {
+    Genero genero;
+
+
+    public Disco(String titulo, String autor, String compania, Genero genero, int duracion, Formato formato) {
         super(titulo, autor, duracion, formato);
         this.compania = compania;
+        this.genero = genero;
     }
 
     @Override
     public String toString() {
-        return "El disco " + "\"" + getTitulo() + "\"" + " del autor/es " + getAutor() + " grabado por la compañía " + compania + " tiene una duración de " + getDuracion() + " minutos y el formato multimedia es " + formato;
+        return "El disco " + "\"" + getTitulo() + "\"" + " de género musical " + genero + " del autor/es " + getAutor() + " grabado por la compañía " + compania + " tiene una duración de " + getDuracion() + " minutos y el formato multimedia es " + formato;
     }
 }
